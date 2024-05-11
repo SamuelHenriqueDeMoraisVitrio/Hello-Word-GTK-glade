67baf2e73eb9134c892152e9f816e1c5a2f23dfc
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-05-11 18:34:34 -0300

    olaaa no terminal

--- gtkHll.c
+++ gtkHll.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <gtk/gtk.h>
+#include <stdio.h>
 
-int main(int numArgumentos, char *nomArgumentos[]){
+int main(int numArgumentos, char *nomArgumentos[]) {
   GtkBuilder *arquivo;
   GtkWidget *obj_tela;
 
@@ -9,11 +9,13 @@ int main(int numArgumentos, char *nomArgumentos[]){
 
   arquivo = gtk_builder_new_from_file("Hello-Word.glade");
 
-  obj_tela = GTK_WIDGET(gtk_builder_get_object(arquivo ,"tela"));
+  obj_tela = GTK_WIDGET(gtk_builder_get_object(arquivo, "tela"));
 
   g_signal_connect(obj_tela, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
   gtk_widget_show(obj_tela);
 
   gtk_main();
+
+  printf("\n\tolaaaaa\n");
 }
